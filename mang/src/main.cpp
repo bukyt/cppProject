5506f5d189da1926290dc9353aba4294e465cc91
Joonas Tiitson <joonastiitson@gmail.com>
2025-04-22 13:50:03 +0300

    worked with the tutorial

--- mang/src/main.cpp
+++ mang/src/main.cpp
@@ -1,23 +1,33 @@
 #include <cute.h>
-
 using namespace Cute;
+
+void mount_content_directory_as(const char* dir)
+{
+    CF_Path path = fs_get_base_directory();
+    path.normalize();
+    path.pop(2); // Pop out of build/debug/
+    path += "/content";
+    fs_mount(path.c_str(), dir);
+}
+
 int main(int argc, char* argv[])
 {
-    // Create a window with a resolution of 640 x 480.
-    CF_Result result = cf_make_app("Fancy Window Title", 0, 0, 0, 640, 480, CF_APP_OPTIONS_WINDOW_POS_CENTERED_BIT, argv[0]);
-    if (is_error(result)) {
-        printf("Error: %s\n", result.details);
-        return -1;
+    mount_content_directory_as(".");
+    CF_Result result = make_app("Basic Sprite", 0, 0, 0, 640, 480, CF_APP_OPTIONS_WINDOW_POS_CENTERED_BIT, argv[0]);
+    if (is_error(result)) return -1;
+
+    CF_Sprite girl_sprite = cf_make_demo_sprite();
+    girl_sprite.animation = girl_sprite.animations[0];
+    girl_sprite.scale = V2(4, 4);
+    cf_sprite_play(&girl_sprite, "idle");
+    while (app_is_running()) {
+        app_update();
+        cf_sprite_update(&girl_sprite);
+        cf_draw_sprite(&girl_sprite);
+        app_draw_onto_screen();
     }
 
-    while (cf_app_is_running())
-    {
-        cf_app_update(NULL);
-        // All your game logic and updates go here...
-        cf_app_draw_onto_screen(true);
-    }
-
-    cf_destroy_app();
+    destroy_app();
 
     return 0;
 }
